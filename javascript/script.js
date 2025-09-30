260ba97d77679a6576d66b389780de5c499e924a
RamiroSabetta <155183794+RamiroSabetta@users.noreply.github.com>
2025-09-30 18:23:04 -0300

    fix card acordeon

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+
+// ===== Acordeón "Problemas Ambientales" =====
+const accordionItems = document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach(item => {
+  const header = item.querySelector(".accordion-header");
+
+  header.addEventListener("click", () => {
+    // Cierra los demás
+    accordionItems.forEach(i => {
+      if (i !== item) {
+        i.classList.remove("active");
+      }
+    });
+
+    // Alterna el actual
+    item.classList.toggle("active");
+  });
+});
+
